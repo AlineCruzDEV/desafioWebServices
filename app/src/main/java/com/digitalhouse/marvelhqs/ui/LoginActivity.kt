422f6e2c3fcd0cdf35344793ee85e8556e1c565f
AlineCruzDEV <linegcruz@gmail.com>
2020-11-29 19:45:26 -0300

    Criacao de Splash Screen

---
+++ app/src/main/java/com/digitalhouse/marvelhqs/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.digitalhouse.marvelhqs.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.digitalhouse.marvelhqs.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
